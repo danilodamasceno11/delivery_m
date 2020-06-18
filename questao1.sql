ce4713707d45922875784b303e6d54b5ab81a56c
danilodamasceno11 <37296498+danilodamasceno11@users.noreply.github.com>
2020-06-18 11:35:20 -0300

    Update questao1.sql

--- questao1.sql
+++ questao1.sql
@@ -1,21 +1,21 @@
 # Consultas realizadas no MySQL
 
-/* 1)a
+# 1)a
 select distinct name,
 room_type  
 from ab_nyc_2019 
 group by name,room_type;
-*/
 
-/* 1)b)
+
+# 1)b)
 select 
 Sum(number_of_reviews) as total,
 avg(reviews_per_month) as media # média das médias de avalicao por mes
 from ab_nyc_2019
 where neighbourhood_group like "Brooklyn" and number_of_reviews > 0
-*/
 
-/* 1)c) 
+
+# 1)c) 
 select name,
 availability_365 as disponibilidade,
 max(price) as preco_maximo,
@@ -25,5 +25,4 @@ from ab_nyc_2019
 where name like '% room %' # contem espaço para pegar literalmente a palavra room
 and availability_365 > 0 
 group by availability_365, name;
-*/
 
